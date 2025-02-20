b688b28b9d73c1f4c71e8a93d3f1dc81a0be3b0b
MichalO <m.olewnik@hotmail.com>
2025-02-20 16:19:30 +0100

    # TEST CASE 2,3,4,5 - Wait for alert to be present 2- Handling confirmation dismissal with waiting 3- Wait for element to be visible 4- Wait for element to be Invisible 5- Wait for element to be enabled

--- Play1_automationcamp/Play1_automationcamp.ir.py
+++ Play1_automationcamp/Play1_automationcamp.ir.py
@@ -17,25 +17,7 @@ from selenium import webdriver
 BASE_URL = 'https://play1.automationcamp.ir/'
 SUBPAGE_WAIT_CONDITIONS_BUTTON = (By.XPATH, '//a[@href="expected_conditions.html"]')
 
-# TEST CASE 1 - Wait for alert to be present
-#ARRANGE
-driver = webdriver.Chrome()
-driver.get(BASE_URL)
-driver.find_element(*SUBPAGE_WAIT_CONDITIONS_BUTTON).click()
-
-#ACT
-driver.find_element(By.XPATH, "//button[@id='alert_trigger']").click()
-WebDriverWait(driver, 5).until(EC.alert_is_present())
-Alert(driver).accept()
-alert_handled = driver.find_element(By.XPATH, "//span[@id='alert_handled_badge']")
-
-#ASSERT
-assert alert_handled.is_displayed(), "Alert not handled"
-
-#TEARDOWN
-driver.close()
-
-# # TEST CASE 2 - Wait for alert to be present
+# # TEST CASE 1 - Handling browser alert with waiting
 # #ARRANGE
 # driver = webdriver.Chrome()
 # driver.get(BASE_URL)
@@ -53,3 +35,67 @@ driver.close()
 # #TEARDOWN
 # driver.close()
 
+#-------------------
+
+# # TEST CASE 2 - Handling confirmation dismissal with waiting
+# #ARRANGE
+# driver = webdriver.Chrome()
+# driver.get(BASE_URL)
+# driver.find_element(*SUBPAGE_WAIT_CONDITIONS_BUTTON).click()
+#
+# #ACT
+# driver.find_element(By.XPATH, "//button[@id='prompt_trigger']").click()
+# WebDriverWait(driver, 5).until(EC.alert_is_present())
+# Alert(driver).dismiss()
+# confirmation_handled = driver.find_element(By.XPATH, "//h3[@id='confirm_cancelled']")
+#
+# # ASSERT
+# assert confirmation_handled.is_displayed(), "Confirmation not handled"
+
+#-------------------
+
+# # TEST CASE 3 - Wait for element to be visible
+# #ARRANGE
+# driver = webdriver.Chrome()
+# driver.get(BASE_URL)
+# driver.find_element(*SUBPAGE_WAIT_CONDITIONS_BUTTON).click()
+#
+# #ACT
+# driver.find_element(By.XPATH, "//button[@id='visibility_trigger']").click()
+# visible_button = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//button[@id='visibility_target']")))
+#
+# # ASSERT
+# assert visible_button.is_displayed(), "Button is not visible"
+
+#-------------------
+
+# # TEST CASE 4 - Wait for element to be Invisible
+# #ARRANGE
+# driver = webdriver.Chrome()
+# driver.get(BASE_URL)
+# driver.find_element(*SUBPAGE_WAIT_CONDITIONS_BUTTON).click()
+#
+# #ACT
+# driver.find_element(By.XPATH, "//button[@id='invisibility_trigger']").click()
+# # driver.find_element(By.XPATH, "/div[@id='invisibility_target']").click()
+# invisible_button = WebDriverWait(driver, 6).until(EC.invisibility_of_element_located((By.XPATH, "//div[@id='invisibility_target']")))
+#
+# # ASSERT
+# assert not invisible_button.is_displayed(), "Spinner is still visible"
+
+#-------------------
+
+# TEST CASE 5 - Wait for element to be enabled
+#ARRANGE
+driver = webdriver.Chrome()
+driver.get(BASE_URL)
+driver.find_element(*SUBPAGE_WAIT_CONDITIONS_BUTTON).click()
+
+#ACT
+driver.find_element(By.XPATH, "//button[@id='enabled_trigger']").click()
+# driver.find_element(By.XPATH, "/div[@id='invisibility_target']").click()
+enabled_button = WebDriverWait(driver, 6).until(EC.element_to_be_clickable((By.XPATH, "//button[@id='enabled_target']")))
+
+# ASSERT
+assert enabled_button.is_enabled(), "Button is still disabled"
+#
