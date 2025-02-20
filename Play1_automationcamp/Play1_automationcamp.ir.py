09ca194fc9993195f22288a7913ef04aa942dd0b
MichalO <m.olewnik@hotmail.com>
2025-02-20 08:29:36 +0100

    # TEST CASE 1 - Wait for alert to be present

--- Play1_automationcamp/Play1_automationcamp.ir.py
+++ Play1_automationcamp/Play1_automationcamp.ir.py
@@ -1,4 +1,6 @@
 import time
+
+from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -14,15 +16,21 @@ from selenium import webdriver
 BASE_URL = 'https://play1.automationcamp.ir/'
 SUBPAGE_WAIT_CONDITIONS_BUTTON = (By.XPATH, '//a[@href="expected_conditions.html"]')
 
-# TEST CASE 1
-#SETUP
+# TEST CASE 1 - Wait for alert to be present
+#ARRANGE
 driver = webdriver.Chrome()
 driver.get(BASE_URL)
-
-#TEST
 driver.find_element(*SUBPAGE_WAIT_CONDITIONS_BUTTON).click()
-time.sleep(5)
+
+#ACT
+driver.find_element(By.XPATH, "//button[@id='alert_trigger']").click()
+WebDriverWait(driver, 5).until(EC.alert_is_present())
+Alert(driver).accept()
+alert_handled = driver.find_element(By.XPATH, "//span[@id='alert_handled_badge']")
+print(alert_handled)
+#ASSERT
+assert EC.visibility_of_element_located(alert_handled)
 
 #TEARDOWN
-driver.close()
+# driver.close()
 
